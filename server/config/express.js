e4f6e2ffe64206689d02ea76269543d5c6cadeb7
Swati Mukherjee <swati@cronj.com>
2017-02-13 10:50:59 +0530

    after gitignoring the node modules

--- server/config/express.js
+++ server/config/express.js
@@ -1,6 +1,5 @@
 var express= require('express');
 var path = require('path');
-
 var public= path.resolve(__dirname + '/../../client/public');
 var init=function(app){
 	app.use(express.static(public))
