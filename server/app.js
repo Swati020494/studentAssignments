90951285682fea758caa69ef3cc46d9d98ee1760
Swati Mukherjee <swati@cronj.com>
2017-02-13 16:10:50 +0530

    create, insert, delete, select query working in postgress

--- server/app.js
+++ server/app.js
@@ -1,7 +1,28 @@
 var express=require('express')
 var app=express();
 var getport=require('./config');
-require("./config/express")(app);
-var route=require("./routes/routes");
-route.init(app);
+var pg=require('pg');
+const client = new pg.Client({user: 'postgres', database :'test' , password : 'database'});
+client.connect(function(err){
+	if(err)	throw err ;
+	client.query('create table if not exists student(id integer primary key , name varchar(20), section char)',function(err,result){
+		if(err) throw err ; 
+		
+	});
+	//DELETE FROM STUDENT WHERE WHERE ID = '6;
+
+	client.query("DELETE FROM STUDENT  WHERE ID = 6",function(err,result){
+		if(err) throw err ; 
+		
+	});
+	var query = client.query("SELECT * FROM student WHERE section = 'A';", function(err,result){
+		if(err) throw err ; 
+		console.log(result);
+		var len = result.length; 
+		console.log(result.rows);
+		client.end(function(err){
+			if(err) throw err ; 
+		});
+	});
+});
 app.listen(getport.port);
--- server/config/express.js
+++
@@ -1,7 +0,0 @@
-var express= require('express');
-var path = require('path');
-var public= path.resolve(__dirname + '/../../client/public');
-var init=function(app){
-	app.use(express.static(public))
-}
-module.exports=init
